8cfa36ebf19bd3a0c0099332d262ad7886d3fd6f
harrydoe999 <135303425+harrydoe999@users.noreply.github.com>
2026-06-05 23:08:55 +0000

    Add Deno installation to build script
    Install Deno and update PATH in build script.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-apt-get install -y ffmpeg
+curl -fsSL https://deno.land/install.sh | sh
+export DENO_INSTALL="/opt/render/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
 pip install -r requirements.txt
